f9a4ff15fb4e7bc79d0bf8721808eb34703ec742
Ravi Rathore <ravi.rathore@joshsoftware.com>
2022-08-01 12:32:34 +0530

    exceptionHandling

---
+++ src/com/josh/java/training/exceptionhandling/InputWrongNumberException.java
@@ -0,0 +1,15 @@
+package com.josh.java.training.exceptionhandling;
+
+
+public class InputWrongNumberException extends Exception {
+
+	public InputWrongNumberException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public InputWrongNumberException(String errorMessage) {
+		super(errorMessage);
+		// TODO Auto-generated constructor stub
+}
+	
+}
